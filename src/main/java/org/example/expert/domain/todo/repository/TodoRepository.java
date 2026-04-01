78df4bd83e13a388e1e1bb6ca1956dcf69737fb9
thetana <zxsa135@naver.com>
2026-04-01 11:57:24 +0900

    fix: 3, 4, 5, 6, 7, 8 수행

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,15 +7,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoCustomRepository {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
-    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
-
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
+    @Query("""
+        SELECT t
+          FROM Todo t
+          LEFT JOIN FETCH t.user u
+         WHERE (:weather IS NULL OR t.weather = :weather)
+           AND (:sdt IS NULL OR t.modifiedAt >= :sdt)
+           AND (:edt IS NULL OR t.modifiedAt <= :edt)
+         ORDER BY t.modifiedAt DESC
+    """)
+    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable, String weather, LocalDateTime sdt, LocalDateTime edt);
 }
